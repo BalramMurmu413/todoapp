7290af9a062fda9d516256ab0d6cb40740477257
Balram Murmu <balramm413@gmail.com>
2024-05-12 23:18:27 +0530

    ready to deploy

--- script.js
+++ script.js
@@ -1,16 +1,72 @@
 
+let form = document.querySelector('form');
+
+form.addEventListener('submit',(event) => {
+    let name = event.target.uname.value;
+    // let number = event.target.unumber.value;
+    // let email = event.target.uemail.value;
+
+    let userData= JSON.parse(localStorage.getItem('userDetails')) ?? []
+
+    userData.push({
+        "name": name
+        // "number": number,
+        // "email": email
+    })
+    
+    localStorage.setItem('userDetails', JSON.stringify(userData))
+    event.target.reset()
+    displayData()
 
 
 
-let userDetails = document.querySelector("form").addEventListener("submit", (event) => {
-    let userName= document.querySelector(".userName").value
-    let userNumber= document.querySelector(".userNumber").value
-    let UserEmail= document.querySelector(".userEmail").value
-    console.log(userName, userNumber, UserEmail)
-    event.preventDefault()
-    reset(FormData)
+    event.preventDefault();
 })
- 
 
-localStorage.setItem("user" , userDetails)
+
+
+let taskCard = document.querySelector(".output-child")
+
+let displayData = ()=>{
+    let userData= JSON.parse(localStorage.getItem('userDetails')) ?? []
+// console.log(userData)
+let result = ""
+userData.forEach((element, index )=> {
+    result += 
+   `  <div class="task-card">
+   <h2>${element.name}</h2>
+   <div>
+   
+   <span class="remove-btn" onclick="remove(${index})">&#10060</span>
+   </div>
+</div>
+`
+});
+
+taskCard.innerHTML= result
+}
+    
+const done =(ele)=>{
+    element.innerHTML = red
+    ele.document.body.style.textDecoration="underline"
+}
+    
+const remove =(index)=>{
+    let userData= JSON.parse(localStorage.getItem('userDetails')) ?? []
+    userData.splice(index, 1)
+    localStorage.setItem('userDetails', JSON.stringify(userData))
+    displayData()
+    
+
+
+}
+    
+const clearAll =()=>{
+    let userData= JSON.parse(localStorage.getItem('userDetails')) ?? []
+    userData.splice(0, userData.length)
+    localStorage.setItem('userDetails', JSON.stringify(userData))
+    displayData()
+}
+
+displayData()
 
